6d6e53c585c77d2f74e5b8fdb880c726d21ae513
Vít Rozsíval <vit.rozsival@apitree.cz>
2026-06-23 22:48:51 +0200

    chore: update ESLint configuration and Renovate settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,1 @@
-import { base } from '@apitree.cz/eslint-config';
-import { defineConfig } from 'eslint/config';
-
-export default defineConfig([
-  ...base,
-  {
-    files: ['.github/**/*.test.js'],
-    rules: {
-      'import/no-extraneous-dependencies': 'off',
-    },
-  },
-]);
+export { base as default } from '@apitree.cz/eslint-config';
